b75df852a65a893b0757d9400ed64cd84f8ad7bc
keni-xi <2352991964@qq.com>
2019-10-29 19:00:14 +0800

    '小程序云开发'

---
+++ node/spider-ajax-content/puppeteer.js
@@ -0,0 +1,16 @@
+// 无头浏览器
+// 软件自动化测试
+// cnpm i puppeteer -$
+const puppeteer=require('puppeteer');
+async function run(){
+    const browser =await puppeteer.launch({
+        headless:false
+    })
+    const page=await browser.newPage();
+    await page.goto('https://juejin.im/books',{
+        waitUntil:'networkidle0'
+    })
+    await page.screenshot({
+        path:'./books.png'
+    })
+}
